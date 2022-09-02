a55ff6fe9440cbc913ad7ad78b556f3c126afeb8
SuperKishROX <selvakishoreofficial@gmail.com>
2022-09-02 22:46:02 +1000

    Edited Claim Management
    displayClaim.svelte
    
    - added the status claimid date table

--- src/routes/admin/claims/displayClaim.svelte
+++ src/routes/admin/claims/displayClaim.svelte
@@ -2,3 +2,48 @@
 
 
 </script>
+
+<svelte:head>
+	<title>Claim Management</title>
+</svelte:head>
+
+<main class="h-full pb16 overflow-y-auto">
+    <div class="container px-6 mx-auto grid">
+      <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Claim Management</h2>
+      <div class= "px-4 py-3 mb-6 bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div id="status" class="p-5 flex flex-col flex-wrap gap-5">
+            <table class="w-full overflow-x-auto">
+                <thread>
+                    <tr
+							class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+							<th class="px-6 py-6 text-base">STATUS</th>
+							<th class="px-6 py-6 text-base">CLAIM ID</th>
+							<th class="px-6 py-6 text-base">DATE</th>
+							
+					</tr>
+                </thread>
+            </table>   
+
+            <!--Countainer-->
+            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+
+            </div>
+            <!--Countainer-->
+            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+
+            </div>
+            <!--Countainer-->
+            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+
+            </div>
+                          
+        </div>
+      </div>
+    </div> 
+    
+<!--New Status-->
+<div class="container px-6 mx-auto grid">
+    
+
+</div>
+</main>
