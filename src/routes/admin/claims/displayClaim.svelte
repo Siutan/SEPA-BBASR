0500bfb30d816917436219f375ecdd6c2a67b03b
SuperKishROX <selvakishoreofficial@gmail.com>
2022-09-04 21:44:16 +1000

    Update
    displayClaim.svelte:
    
    1) removed the tables
    2) Added everything in divs

--- src/routes/admin/claims/displayClaim.svelte
+++ src/routes/admin/claims/displayClaim.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-
+    let claim = [
+        {}
+    ]
 
 </script>
 
@@ -7,36 +9,62 @@
 	<title>Claim Management</title>
 </svelte:head>
 
-<main class="h-full pb16 overflow-y-auto">
+<main class="h-full overflow-y-auto">
     <div class="container px-6 mx-auto grid">
       <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Claim Management</h2>
       <div class= "px-4 py-3 mb-6 bg-white rounded-lg shadow-md dark:bg-gray-800">
-        <div id="status" class="p-5 flex flex-col flex-wrap gap-5">
-            <table class="w-full overflow-x-auto">
-                <thread>
-                    <tr
-							class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-							<th class="px-6 py-6 text-base">STATUS</th>
-							<th class="px-6 py-6 text-base">CLAIM ID</th>
-							<th class="px-6 py-6 text-base">DATE</th>
-							
-					</tr>
-                </thread>
-            </table>   
+        <div class="grid gap-15 mb-10 grid-cols-2">
 
-            <!--Countainer-->
+         <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800"> 
             <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-
-            </div>
-            <!--Countainer-->
-            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-
-            </div>
-            <!--Countainer-->
-            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-
-            </div>
-                          
+           <!--Status-->
+				<div>
+					<p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400 px-8 py-3">STATUS</p>	
+				</div>
+            <!--ClaimID-->
+				<div>
+					<p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400 px-8 py-3">CLAIM ID</p>	
+				</div>
+            <!--Date-->
+				<div>
+					<p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400 px-8 py-3">DATE</p>	
+				</div>
+         </div>        
+         <div class="flex items-center justify-between p-2 mb-8 text-sm font-semibold bg-yellow-600 rounded-lg">
+                <!--Second Box-->
+                <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                    <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                        <!--Status-->
+                             <div>
+                                 <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400 px-10 py-3">STATUS</p>
+                                 <p class="px-8 py-3 text-sm text-green-400 ">SUCCESS</p>	
+                             </div>
+                         <!--ClaimID-->
+                             <div>
+                                 <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400 px-10 py-3">CLAIM ID</p>
+                                 <p class="px-8 py-3 text-sm">12345678</p>		
+                             </div>
+                         <!--Version-->
+                             <div>
+                                 <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400 px-10 py-3">VERSION</p>
+                                 <p class="px-16 py-3 text-sm">1</p>	
+                             </div>
+                         <!--DATE-->
+                             <div>
+                                 <p class="mb-3 text-sm font-medium text-gray-600 dark:text-gray-400 px-10 py-3">DATE</p>	
+                                 <p class="px-4 py-3 text-sm">01/01/2022</p>
+                             </div>
+                         <!--CID-->
+                             <div>
+                                <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400 px-4 py-3">CID</p>
+                                <p class="px-3 py-3 text-sm">SUCCESS</p>	
+                            </div>
+                    </div>
+                        
+                </div>
+                
+			
+        </div>              
         </div>
       </div>
     </div> 
