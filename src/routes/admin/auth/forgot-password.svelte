e01260a30d7634a9b88248ddd0f74dc29bdb35ac
Mukil Chittybabu <Mukil033@gmail.com>
2022-09-10 17:38:07 +1000

    Update forgot-password.svelte

--- src/routes/admin/auth/forgot-password.svelte
+++ src/routes/admin/auth/forgot-password.svelte
@@ -10,20 +10,6 @@
       class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800"
     >
       <div class="flex flex-col overflow-y-auto md:flex-row">
-        <div class="h-32 md:h-auto md:w-1/2">
-          <img
-            aria-hidden="true"
-            class="object-cover w-full h-full dark:hidden"
-            src={forgotPasswordOffice}
-            alt="Office"
-          />
-          <img
-            aria-hidden="true"
-            class="hidden object-cover w-full h-full dark:block"
-            src={forgotPasswordOfficeDark}
-            alt="Office"
-          />
-        </div>
         <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
           <div class="w-full">
             <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
