cde3362561f5b05492b2896c4b6e7ac43f729461
Mukil Chittybabu <Mukil033@gmail.com>
2022-09-12 17:04:33 +1000

    Update manageClaim.svelte

--- src/routes/admin/claims/manageClaim.svelte
+++ src/routes/admin/claims/manageClaim.svelte
@@ -92,7 +92,7 @@
 
 <main class="h-full pb-16 overflow-y-auto">
   <div class="container px-6 mx-auto grid">
-    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Manage Users</h2>
+    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Manage Claims</h2>
     <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
       {#await getClaims()}
         <!-- loading -->
