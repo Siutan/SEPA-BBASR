fa89b364160075b3e19b6c8895f1e78315a0665b
SuperKishROX <selvakishoreofficial@gmail.com>
2022-09-12 15:39:43 +1000

    displayclaim
    - separated and fixed the flex layout

--- src/routes/admin/claims/displayClaim.svelte
+++ src/routes/admin/claims/displayClaim.svelte
@@ -13,18 +13,15 @@
     <div class="container px-6 mx-auto grid">
       <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Claim Management</h2>
       <div class= "px-4 py-3 mb-6 bg-white rounded-lg shadow-md dark:bg-gray-800">
-        <button
-						
-						class=" mx-5 px-5 py-3 font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-					>
-						Data
-					</button>
-         <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 justify-around"> 
+        <button class=" mx-5 px-5 py-3 font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">Data</button>
+         <div class="flex items-center p-4 dark:bg-gray rounded-lg shadow-xs white-800 justify-around"> 
+             
             
-            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 ">
+            <div class="flex flex-auto items-center p-5 bg-white rounded-lg shadow-xs dark:bg-gray-800 ">
            <!--Status-->
-				<div>
+				<div >
 					<p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400 px-8 py-3">STATUS</p>	
+                  
 				</div>
             
             <!--ClaimID-->
@@ -36,13 +33,18 @@
 					<p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400 px-8 py-3">DATE</p>	
 				</div>
                 
-         </div>       
+                
+                
+         </div>   
+         
+         
+        
 
-         <div class="flex items-center justify-between p-0.5 mb-8 text-sm font-semibold bg-white rounded-lg">
+         <div class="flex flex-auto items-center justify-between p-0.5 mb-8 text-sm font-semibold bg-white rounded-lg">
                 <!--Second Box-->
-                <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                <div class="min-w-0 p-4 dark:bg-gray rounded-lg shadow-xs dark:bg-gray-800">
 
-                    <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+                    <div class="flex items-center p-4 dark:bg-gray shadow-xs dark:bg-gray-800">
                         <!--Status-->
                              <div>
                                  <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400 px-10 py-3">STATUS</p>
