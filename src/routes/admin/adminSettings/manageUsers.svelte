cda2c14f36186926191fef8377b2dfbeae6ad3c4
Mukil Chittybabu <Mukil033@gmail.com>
2022-10-03 15:50:42 +1100

    QoL Changes

--- src/routes/admin/adminSettings/manageUsers.svelte
+++ src/routes/admin/adminSettings/manageUsers.svelte
@@ -166,6 +166,20 @@
       {:then data}
         <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-2">
           <div class="w-full overflow-x-auto col-span-2 ">
+            <div>
+              <button
+                class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-full active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                aria-label="Edit"
+                on:click={openModal}
+              >
+                <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                    d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                  />
+                </svg>
+                <span>Add New User</span>
+              </button>
+            </div>
             <table class="w-full whitespace-no-wrap">
               <thead>
               <tr
